5e2d2eeb7ab2221f98ef752bddcddee1441a4418
schfranz <schfranz93@gmail.com>
2018-12-09 18:39:22 -0800

    properly sources other files

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
+#TODO: determine way to automatically set working directory to location of this file
 
-
-#run other scripts to make load all necessary functions into the workspace (this needs to go elsewhere in the package)
-source('~/FranziskaSchool/STAT243/finalProject/main.R')
-
-
-
+#source other scripts to load all necessary functions into the workspace; TODO: fail more gracefully if files aren't present
+source("arsFunction.R") #, local = TRUE, chdir = TRUE) <- other options for source() that could potentially help automatically setting working directory
+source("supportingFunctions.R")
+source("testFunctions.R")
+source("testSuite.R")
