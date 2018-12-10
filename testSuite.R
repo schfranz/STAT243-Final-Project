bfde44530acbb038db26ef168c8b556925c2a6f9
schfranz <schfranz93@gmail.com>
2018-12-09 18:08:29 -0800

    deleted irrelevant line

--- testSuite.R
+++ testSuite.R
@@ -6,8 +6,5 @@ assertthat::assert_that(check_f_positive(dnorm,-100,100))
 
 
 #sample test
-h <- function(x){
-	return(log(g(x)))
-}
 g <- function(x) dnorm(x,0,1)
 x <- ars(g,5000,-10,10,0,batch_size=100)
