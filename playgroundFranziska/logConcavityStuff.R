c905cb475fbf03c4610ac866489865f304a00371
schfranz <schfranz93@gmail.com>
2018-12-10 12:12:14 -0800

    added a list of functions that are not log-concave

--- playgroundFranziska/logConcavityStuff.R
+++ playgroundFranziska/logConcavityStuff.R
@@ -1,6 +1,6 @@
 #playing with log-concavity
 
-#log-concavity determined by if F(alpha*x + (1-alpha*y)) >= F(x)^alpha * F(y)^(1-alpha) (Davis paper)
+#log-concavity determined by if F(alpha*x + (1-alpha)*y) >= F(x)^alpha * F(y)^(1-alpha) (wrong in Davis paper)
 #example with dnorm, should return all TRUE
 alpha <- 0.5
 x <- seq(100)
@@ -8,3 +8,14 @@ y <- x
 hm <- dnorm(alpha*x + (1 - alpha*y))
 hm2 <- dnorm(x)^alpha * dnorm(y)^(1-alpha)
 hm >= hm2
+
+
+#functions that are non-log-concave according to Wikipedia:
+	#Student's t
+	#Cauchy
+	#Pareto
+	#log-normal
+	#F-distribution
+#some non-log-concave functions may have log-concave CDFs, so this is not a good test
+#test with Student's t
+(dt(alpha*x + (1 - alpha)*y,1)) >= (dt(x,1)^alpha * dt(y,1)^(1-alpha))
