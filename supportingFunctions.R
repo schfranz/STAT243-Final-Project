1a867fdacc7ba01cda2146f36d2d303cd7264bec
yuanweijie123 <weijie_yuan@berkeley.edu>
2018-12-10 21:34:30 -0800

    supporting

--- supportingFunctions.R
+++ supportingFunctions.R
@@ -1,122 +1,132 @@
 #supporting functions (might want to make one .R script per function in package)
-library(numDeriv)
 
 h <- function(x){
-	return(log(g(x)))
+  return(log(g(x)))
 }
 
-# generate intersect z_j
-generate_intersect <- function(h_k, dh_k, x_k, lb, ub){
-	k = length(h_k)
-	x_k_l <- c(x_k,0)
-	x_k_o <- c(0,x_k)
-	h_k_l <- c(h_k,0)
-	h_k_o <- c(0,h_k)
-	dh_k_l <- c(dh_k,0)
-	dh_k_o <- c(0,dh_k)
-	zj <- (h_k_l-h_k_o-x_k_l*dh_k_l+x_k_o*dh_k_o)/(dh_k_o-dh_k_l)
-	zj[1] <- lb
-	zj[k+1] <- ub
-	return(zj)
+# generate intersect zj
+generate_intersect <- function(hk, dhk, xk, lb, ub){
+  k = length(hk)
+  xkl <- c(xk,0); xko <- c(0,xk)
+  hkl <- c(hk,0); hko <- c(0,hk)
+  dhkl <- c(dhk,0); dhko <- c(0,dhk)
+  zj <- (hkl-hko-xkl*dhkl+xko*dhko)/(dhko-dhkl)
+  zj[1] <- lb
+  zj[k+1] <- ub
+  return(zj)
 }
+
+
 
 
 # initialization
 initialization_step <- function(h, lb, ub){
-	delta <- (ub - lb)/500
-	max_point <- optimize(f = h, interval = c(lb, ub), lower = lb, upper = ub, maximum = TRUE)$maximum
-	
-	if (abs(max_point - ub) < 1e-5) {
-		right_point <- max_point
-		mid_point <- max_point - delta/2
-		left_point <- (max_point - delta)
-	} else if (abs(max_point - lb) < 1e-5) {
-		right_point <- (max_point + delta)
-		mid_point <- max_point + delta/2
-		left_point <- max_point
-	} else {
-		right_point <- (max_point + delta)
-		mid_point <- max_point
-		left_point <- (max_point - delta)
-	}
-	
-	init <- c(left_point, mid_point, right_point)
-	return(init)
+  
+  # considering lb and ub is infinity
+  if (lb==-Inf | ub==Inf){
+    maxPoint <- optim(par=0, f = h, method = "L-BFGS-B", 
+                      lower = lb, upper = ub, control=list(fnscale=-1))$par
+  } else {
+    maxPoint <- optimize(f = h, interval = c(lb, ub), 
+                         lower = lb, upper = ub, maximum = TRUE)$maximum
+  }
+  
+  # pre-set interval delta
+  delta <- 1
+  
+  # set three points
+  if (abs(maxPoint - ub) < 1e-5) {
+    rightPoint <- maxPoint
+    midPoint <- maxPoint - delta/2
+    leftPoint <- (maxPoint - delta)
+  } else if (abs(maxPoint - lb) < 1e-5) {
+    rightPoint <- (maxPoint + delta)
+    midPoint <- maxPoint + delta/2
+    leftPoint <- maxPoint
+  } else {
+    rightPoint <- (maxPoint + delta)
+    midPoint <- maxPoint
+    leftPoint <- (maxPoint - delta)
+  }
+  
+  init <- c(leftPoint, midPoint, rightPoint)
+  return(init)
 }
 
 
 # create upper hull in a vectorized fashion
-# take exponential
-upper_hull <- function(x,h_k,dh_k,x_k,z_k){
-	i <- min(which(x < z_k)-1)
-	upperBound <- h_k[i] + (x - x_k[i]) * dh_k[i]
-	return(upperBound)
+upper_hull <- function(x,hk,dhk,xk,zk){
+  i <- min(which(x < zk)-1)
+  upperBound <- hk[i] + (x - xk[i]) * dhk[i]
+  return(upperBound)
 }
 
-exp_upper_hull <- function(x,h_k,dh_k,x_k,z_k){
-	expUpBound <- exp(upper_hull(x,h_k,dh_k,x_k,z_k))
-	return(expUpBound)
+# take exponential values of upper hull
+exp_upper_hull <- function(x,hk,dhk,xk,zk){
+  expUpBound <- exp(upper_hull(x,hk,dhk,xk,zk))
+  return(expUpBound)
 }
 
 
 # create lower hull in a vectorized fashion
-lower_hull <- function(x,h_k,dh_k,x_k){
-	if(x < min(x_k) | x > max(x_k)){
-		return(-Inf)
-	}else{
-		i <- max(which(x >= x_k))
-		lowerBound <- ((x_k[i + 1] - x) * x_k[i] + (x - x_k[i]) * h_k[i + 1])/(x_k[i + 1] - x_k[i])
-		return(lowerBound)
-	}
+lower_hull <- function(x,hk,dhk,xk){
+  if(x < min(xk) | x > max(xk)){
+    return(-Inf)
+  }else{
+    i <- max(which(x >= xk))
+    lowerBound <- ((xk[i + 1] - x) * xk[i] + (x - xk[i]) * hk[i + 1])/(xk[i + 1] - xk[i])
+    return(lowerBound)
+  }
 }
 
 
 # sample from the envelope
-draw_sample <- function(u, cum_env, h_k, x_k, dh_k, z_k, portion){
-	
-	j <- max(which(u > cum_env))
-	
-	if(dh_k[j] == 0){
-		x <- runif(1, z_k[j],z_k[j+1])
-		return(x)
-	} else {
-		
-		# Sample from uniform random
-		w = runif(1)
-		
-		# Rescale sample value w to area of the selected segment, since area under segment is not equal to 1
-		w_sc = w/portion[j]*exp(h_k[j] - x_k[j]*dh_k[j])*(exp(dh_k[j]*z_k[j+1]) -exp(dh_k[j]*z_k[j]))
-		
-		# Use inverse CDF of selected segment to generate a sample
-		x = (1/dh_k[j])*log(w_sc*portion[j]/(exp(h_k[j] - x_k[j]*dh_k[j])) + exp(z_k[j]*dh_k[j]))
-	}
-	return(x)
+draw_sample <- function(u, cumEnv, hk, xk, dhk, zk, portion){
+  
+  j <- max(which(u > cumEnv))
+  
+  if(dhk[j] == 0){
+    x <- runif(1, zk[j],zk[j+1])
+    return(x)
+  } else {
+    
+    # Sample from uniform random
+    w = runif(1)
+    
+    # Rescale sample value w to area of the selected segment
+    wRescale = w/portion[j]*exp(hk[j] - xk[j]*dhk[j])*(exp(dhk[j]*zk[j+1]) -exp(dhk[j]*zk[j]))
+    
+    # Use inverse CDF of selected segment to generate a sample
+    x = (1/dhk[j])*log(wRescale*portion[j]/(exp(hk[j] - xk[j]*dhk[j])) + exp(zk[j]*dhk[j]))
+  }
+  return(x)
 }
 
 #rejection test
-rejection_test <- function(x, h_k, dh_k, x_k, z_k){
-	# Generate random seed
-	w = runif(1)
-	
-	# squeeze and reject tests indicator for adding point in boolean form
-	accept = FALSE
-	add = FALSE
-	
-	# get rejection point for squeeze and accept test
-	lower_test = exp(lower_hull(x,h_k,dh_k,x_k) - upper_hull(x,h_k,dh_k,x_k,z_k))
-	
-	if(w <= lower_test){
-		accept = TRUE
-	} else {
-		add = TRUE
-		function_test = exp(h(x) - upper_hull(x,h_k,dh_k,x_k,z_k))
-		if(w <= function_test){
-			accept = TRUE
-		}else{
-			accept = FALSE
-		}
-	}
-	
-	# Return boolean indicator whether to accept candidate sample point
-	return(list(acceptIndicator = accept , UpdateIndicator = add))
+rejection_test <- function(x, hk, dhk, xk, zk){
+  
+  # Generate random sample from uniform distribution
+  w = runif(1)
+  
+  # squeeze and reject tests indicator for adding point in boolean form
+  accept = FALSE
+  add = FALSE
+  
+  # get rejection point for squeeze and accept test
+  lowerTest = exp(lower_hull(x,hk,dhk,xk) - upper_hull(x,hk,dhk,xk,zk))
+  
+  if(w <= lowerTest){
+    accept = TRUE
+  } else {
+    add = TRUE
+    functionTest = exp(h(x) - upper_hull(x,hk,dhk,xk,zk))
+    if(w <= functionTest){
+      accept = TRUE
+    }else{
+      accept = FALSE
+    }
+  }
+  
+  # Return boolean indicator whether to accept candidate sample point
+  return(list(acceptIndicator = accept , UpdateIndicator = add))
 }
