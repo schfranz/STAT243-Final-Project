9b4234de63415498185ef0e184a2ab0c9268dd8d
schfranz <schfranz93@gmail.com>
2018-12-12 14:26:39 -0800

    added tests for chi-squared, deleted Fdist test (function comes from other packages, too messy)

--- ars/inst/tests/testthat/testArsOutputs.R
+++ ars/inst/tests/testthat/testArsOutputs.R
@@ -20,6 +20,13 @@ testthat::test_that("test with gamma(2,2) distribution", {
 	testthat::expect_gte(p$p.value, 0.1)
 })
 
+testthat::test_that("test with dchisq(3) distribution", {
+	f <- function(x) dchisq(x, 3)
+	samples <- ars(f, 1000, 0, Inf)
+	p <- ks.test(samples, 'pgamma', 2, 2)
+	testthat::expect_gte(p$p.value, 0.1)
+})
+
 
 #tests to check that non-log-concavity is detected
 testthat::test_that("test with t-distribution with 1 degree of freedom", {
@@ -37,7 +44,7 @@ testthat::test_that("test with default log-normal distribution", {
 	testthat::expect_error(ars(f, 1000, 0, Inf), "non-log-concave")
 })
 
-testthat::test_that("test with default F distribution with 1 degree of freedom", {
-	f <- function(x) FDist(x, 1)
+testthat::test_that("test with chi-squared distribution with 1 degree of freedom", {
+	f <- function(x) dchisq(x, 1)
 	testthat::expect_error(ars(f, 1000, 0, Inf), "non-log-concave")
 })
